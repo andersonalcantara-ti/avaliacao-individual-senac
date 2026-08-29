e75a1fe8a4065bc2daa8380941797070f579d169
Anderson Alcantara <andersonalcantara.ti@gmail.com>
2026-08-29 10:11:21 -0300

    feat: cria novo repositorio
    Repositório das avaliações individuais do curso de Pós-Graduação em Desenvolvimento Web do SENAC SP.

---
+++ linguagens-de-interacao/questao-01/questao-01.js
@@ -0,0 +1,15 @@
+/*
+QUESTÃO 01:
+---
+Imagine que você está planejando a criação de uma funcionalidade para um site de e-commerce. A tarefa é organizar as informações de um vitrine de produtos e descrever como funcionaria ma consulta de estoque. Inicialmente, um colega de equipe sugeriu que, para cada produto, fosse criada uma descrição separada e um texto de verificação de estoque individual. Você, no entanto, acredita que uma abordagem mais estruturada, baseada nos conceitos de Funções, Objetos e Arrays, seria muito mais eficiente.
+---
+
+Com base neste cenário, elabore uma resposta que contemple os seguintes pontos:
+Repetir a mesma lógica várias vezes pode tornar um projeto complexo e difícil de manter. Explique, com suas palavras, por que a abordagem de criar uma função (um bloco de código reutilizável) é superior a simplesmente repetir a mesma lógica para cada produto.
+---
+
+Um objeto em JavaScript permite agrupar várias características (propriedades) sobre um mesmo item, como um carro que tem cor e portas. Um array é uma lista que pode guardar vários desses itens, como uma concessionária que tem uma lista de carros. Descreva em formato de texto ou lista como você organizaria as informações de três produtos diferentes, onde cada produto é um item com as seguintes propriedades: id (número de identificação), nome, cor e preço. Não é preciso escrever código. A ideia é mostrar como você agruparia os dados de forma lógica.
+---
+
+Uma função recebe dados (parâmetros), executa uma tarefa e pode devolver um resultado com o comando return. Descreva, em um passo a passo lógico (sem usar código), como uma função chamada verificaEstoque deveria funcionar. Sua descrição deve explicar que a função recebe um id como entrada e, usando uma estrutura de controle de fluxo, ela deve verificar se o número do id é par ou ímpar para decidir qual das seguintes mensagens será retornada: "Produto disponível em estoque." ou "Produto indisponível no momento.". 
+*/
